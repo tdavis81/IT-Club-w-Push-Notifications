960a82b6ec277135db938a8c1fda2e24a41af3a2
tdavis81 <35125581+tdavis81@users.noreply.github.com>
2019-05-18 21:13:01 -0400

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,13 +5,7 @@ if(!isIphone) {
   importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
 
   const config = {
-    apiKey: "AIzaSyDCG7agWQb9KOHe-aARPLGLwrN7r5260a4",
-    authDomain: "fortnitetv-26656.firebaseapp.com",
-    databaseURL: "https://fortnitetv-26656.firebaseio.com",
-    projectId: "fortnitetv-26656",
-    storageBucket: "fortnitetv-26656.appspot.com",
-    messagingSenderId: "296425214337",
-    appId: "1:296425214337:web:f5495f606032bc2c"
+   
     };
   firebase.initializeApp(config)
 
