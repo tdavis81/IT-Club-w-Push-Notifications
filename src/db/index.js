34c3c7d06a85f62b436f721e2b14a5e85b545633
tdavis81 <35125581+tdavis81@users.noreply.github.com>
2019-05-18 21:12:26 -0400

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -5,13 +5,7 @@ import db from '@/db';
 //const settings = {timestampsInSnapshots: true};
 
 const config = {
-  apiKey: "AIzaSyDCG7agWQb9KOHe-aARPLGLwrN7r5260a4",
-  authDomain: "fortnitetv-26656.firebaseapp.com",
-  databaseURL: "https://fortnitetv-26656.firebaseio.com",
-  projectId: "fortnitetv-26656",
-  storageBucket: "fortnitetv-26656.appspot.com",
-  messagingSenderId: "296425214337",
-  appId: "1:296425214337:web:f5495f606032bc2c"
+  api
   };
 const firebaseapp = firebase.initializeApp(config)
 
@@ -21,7 +15,7 @@ let isIphone = /(iphone)/i.test(navigator.userAgent)
 if (!isIphone) {
   const messaging = firebase.messaging();
 
-  messaging.usePublicVapidKey("BCKasHrDkLoVL9yDZzZ9YpePzVypEPf8pQzDLD3LiltOiQdWPnws0ds_MGjoQsIJSYlNz_mWTQjYyIw5iY6QWJo"); // 1. Generate a new key pair
+  messaging.usePublicVapidKey(""); // 1. Generate a new key pair
 
   // Request Permission of Notifications
   messaging.requestPermission().then(() => {
